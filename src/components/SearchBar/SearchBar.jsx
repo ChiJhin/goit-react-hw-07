769fb1c3da9c9596f9b4557ef071b51ca65f88ef
Dimitry Minchukov <jerek.d.lem@gmail.com>
2024-03-02 00:42:08 +0200

    full update

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,28 @@
+import { useId } from "react";
+import { useDispatch} from "react-redux";
+
+import css from "./SearchBar.module.css";
+import { filterNumbers } from "../../redux/FilterContacts/filterSlice";
+
+
+const SearchBar = () => {
+  const id = useId();
+  const dispatch = useDispatch();
+  const onChange = (e) => dispatch(filterNumbers(e.target.value.trim()));
+
+ 
+  return (
+    <div className={css.searchBar}>
+      <label htmlFor={id}>Find contact by name</label>
+      <input
+        type="text"
+        id={id}
+        className={css.searchInput}
+        onChange={onChange}
+        placeholder="Search"
+        name="search"
+      />
+    </div>
+  );
+};
+export default SearchBar
